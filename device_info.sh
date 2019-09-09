20a919b06625c4d86ff9ccc2b890bc0451ea8743
aben20807 <aben20807@gmail.com>
2019-09-09 17:48:53 +0800

    Fix the spaces in name

--- device_info.sh
+++ device_info.sh
@@ -3,21 +3,22 @@
 sudo apt-get install -y smartmontools > /dev/null
 
 read -p "Enter Your name: " name
-printf "<${name}'s Computer>\n" > device_info_${name}
+output_file=device_info_${name// /_}.txt
+printf "<${name}'s Computer>\n" > ${output_file}
 
-printf "\n--------CPU Info--------\n\n" >> device_info_${name}
-lscpu | grep -E "Model name|Core\(s\) per socket|Socket\(s\)|每通訊端核心數：" >> device_info_${name}
+printf "\n--------CPU Info--------\n\n" >> ${output_file}
+lscpu | grep -E "Model name|Core\(s\) per socket|Socket\(s\)|每通訊端核心數：" >> ${output_file}
 
-printf "\n--------GPU Info--------\n\n" >> device_info_${name}
-sudo lshw -c display | grep -E "product" >> device_info_${name}
+printf "\n--------GPU Info--------\n\n" >> ${output_file}
+sudo lshw -c display | grep -E "product" >> ${output_file}
 
-printf "\n--------RAM Info--------\n\n" >> device_info_${name}
+printf "\n--------RAM Info--------\n\n" >> ${output_file}
 sudo dmidecode --type memory |\
 grep -E "Memory Device|Size:|Type:|Speed:" |\
 grep -v "Clock Speed:" |\
-grep -v "Error" >> device_info_${name}
+grep -v "Error" >> ${output_file}
 
-printf "\n--------Disk Info--------\n\n" >> device_info_${name}
+printf "\n--------Disk Info--------\n\n" >> ${output_file}
 
 disk_device_array=( $(ls /dev/sd* | grep -v "[0-9]") )
 
@@ -25,6 +26,7 @@ for (( i=0; i<${#disk_device_array[@]}; i++ ))
 do
     sudo smartctl -i ${disk_device_array[i]} |\
     grep -E "Model Family:|User Capacity:|Rotation Rate:|SATA Version is:"\
-    >> device_info_${name}
+    >> ${output_file}
     printf "\n"
 done
+printf "Finished. Please upload \"%s\"" ${output_file}
